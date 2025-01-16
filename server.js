ba8a6a6ea489152eaaffca2234fe56db508774cc
AntoineDlb <antoine.delebecque@supinfo.com>
2025-01-16 14:57:38 +0100

    refactor(nom server et README)
    modification de nom de fichier et ajout du README au projet

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Servir les fichiers statiques du dossier "public"
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route pour servir le fichier index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
